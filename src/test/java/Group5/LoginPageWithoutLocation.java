1ab8545de903f1a66469111b739498d53e847858
nurdane <nurdane81@gmail.com>
2023-02-23 20:14:12 -0700

    login page without choosing located updated.

--- src/test/java/Group5/LoginPageWithoutLocation.java
+++ src/test/java/Group5/LoginPageWithoutLocation.java
@@ -3,29 +3,27 @@ package Group5;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class LoginPageWithoutLocation extends UtilityClass {
     public static void main(String[] args) {
-            /*1- Go to "https://demo.openmrs.org/openmrs/login.htm".
-              2- Try to log in with username="Admin" and password="Admin123"
-           without choosing a location and verify that the error
-           message is "You must choose a location!".*/
+        /* Go to "https://demo.openmrs.org/openmrs/login.htm".
+         1- Once you are in Login page
 
+        2- Enter valid username into the “Username” field
+
+        3- Enter valid password in to the “Password” field
+
+        4- Click on “Login” button*/
         driver.get("https://demo.openmrs.org/openmrs/login.htm");
         WebElement logIn = driver.findElement(By.id("username"));
         logIn.sendKeys("Admin" + Keys.TAB + "Admin123" + Keys.ENTER);
         String actualErrorMessage = driver.findElement(By.id("sessionLocationError")).getText();
         String expectedErrorMessage = "You must choose a location!";
         if (expectedErrorMessage.equals(actualErrorMessage)) {
-            System.out.println("Step 2 is Passed");
+            System.out.println("Passed");
         } else {
-            System.out.println("Step 2 is Failed");
+            System.out.println("Failed");
 
         }
         quitDriver(4);
